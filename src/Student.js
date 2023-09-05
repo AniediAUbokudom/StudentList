c114eb2179240ca952f87c9f8233ef95e3413807
Aniedi Ubokudom <aniedi.anthonyu@yahoo.com>
2023-09-04 20:34:36 -0400

    Student-List

---
+++ src/Student.js
@@ -0,0 +1,16 @@
+import Score from './Score';
+
+const Student = ({ student }) => {
+  return (
+    <div>
+      <h2>{student.name}</h2>
+      <p>{student.bio}</p>
+      <h3>Scores:</h3>
+      {student.scores.map((score, index) => (
+        <Score key={index} score={score} />
+      ))}
+    </div>
+  );
+};
+
+export default Student;
